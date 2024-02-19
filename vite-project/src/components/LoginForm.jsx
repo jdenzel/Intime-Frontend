9784db09945c145ef1ccbaed6b91af65c7395a44
Denzel <jdenzelmab@gmail.com>
2024-02-18 22:48:55 -0500

    Added formik with axios.post for handling user validation and form submission for user logins

--- vite-project/src/components/LoginForm.jsx
+++ vite-project/src/components/LoginForm.jsx
@@ -7,3 +7,29 @@ const loginSchema = Yup.object().shape({
     username: Yup.string().required('Required'),
     password: Yup.string().required('Required'),
 })
+
+function LoginForm() {
+    const navigate = useNavigate();
+
+    return (
+        <Formik initialValues={{
+            username: '',
+            password: ''
+        }}
+        validationSechema={loginSchema}
+        onSubmit={(values, {setSubmitting, setErrors}) => {
+            axios.post("https://intime.applikuapp.com/login/", values, { withCredentials:true })
+                .then(response => {
+                    if(response.status === 200) {
+                        navigate('/')
+                    }
+                    setSubmitting(false)
+                    .catch(error => {
+                        setErrors(error.response)
+                        setSubmitting(false)
+                    });
+                })
+        }}>
+        </Formik>
+    )
+}
