e5a196aeff0568084f82209d36231689227838ea
Denzel <jdenzelmab@gmail.com>
2024-02-18 22:54:59 -0500

    Finished with Formik form

--- vite-project/src/components/LoginForm.jsx
+++ vite-project/src/components/LoginForm.jsx
@@ -4,8 +4,8 @@ import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
 
 const loginSchema = Yup.object().shape({
-    username: Yup.string().required('Required'),
-    password: Yup.string().required('Required'),
+    username: Yup.string().required('Username Required'),
+    password: Yup.string().required('Password Required'),
 })
 
 function LoginForm() {
@@ -30,6 +30,19 @@ function LoginForm() {
                     });
                 })
         }}>
+            {({ isSubmitting }) => (
+                <Form>
+                    <h4>Username:</h4>
+                    <Field type="text" name="username"/>
+                    <ErrorMessage name="username" component="p"/>
+                    
+                    <h4>Password:</h4>
+                    <Field type="text" name="password"/>
+                    <ErrorMessage name="password" component="p"/>
+
+                    <button type='submit' disabled={isSubmitting}>Log In</button>
+                </Form>
+            )}
         </Formik>
     )
 }
