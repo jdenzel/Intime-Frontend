51e5b11a1cd2278addc20e62e12ebe26e21efbed
Denzel <jdenzelmab@gmail.com>
2024-02-18 22:17:15 -0500

    Added loginSchema for form validation

--- vite-project/src/components/LoginForm.jsx
+++ vite-project/src/components/LoginForm.jsx
@@ -2,3 +2,8 @@ import  { Formik, Field, Form, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
 import { useNavigate } from 'react-router-dom'
+
+const loginSchema = Yup.object().shape({
+    username: Yup.string().required('Required'),
+    password: Yup.string().required('Required'),
+})
