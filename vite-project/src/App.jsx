c85ab42b23af252b645f650de5e0247045107232
Denzel <jdenzelmab@gmail.com>
2024-02-18 21:30:20 -0500

    imported useState and BrowserRouter. Added an axios request to /checksession endpoint

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    axios.get("https://intime.applikuapp.com/checksession/")
+      .then((r) => {
+        if(r.status === 200)  {
+          setUser(r.data)
+        }
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Routes>
+      
+    </Routes>
   )
+
 }
 
 export default App
