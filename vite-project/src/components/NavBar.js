98c83c872a082c6a058e774bd917d8f258a50761
Denzel <jdenzelmab@gmail.com>
2024-02-20 05:24:13 -0500

    Updated Navbar with logout request

--- vite-project/src/components/NavBar.js
+++ vite-project/src/components/NavBar.js
@@ -2,3 +2,25 @@ import { Link, useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
+function Navbar({ user, setUser })  {
+    const csrfToken = Cookies.get('csrftoken')
+    const navigate = useNavigate()
+
+    const handleLogout = async() => {
+        try {
+            const r = await axios.post('https://dtesting.applikuapp.com/logout/', {}, { 
+                headers: {
+                    'X-CSRFToken': csrfToken
+                }, 
+                withCredentials: true });
+            if(r.status === 204) {
+                setUser(null);
+                navigate('/login');
+            } else {
+                console.error("Logout failed status code: ", r.status);
+            }
+        } catch (error) {
+            console.error("Logout failed", error);
+        }
+    }
+}
