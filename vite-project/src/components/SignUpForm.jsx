5b32e08beddbf8b4e1bf1a936092a309d1be7700
Denzel <jdenzelmab@gmail.com>
2024-02-20 05:09:56 -0500

    Added useNavigate and created Formik with post request to sign up user

--- vite-project/src/components/SignUpForm.jsx
+++ vite-project/src/components/SignUpForm.jsx
@@ -2,7 +2,9 @@ import React from "react"
 import { Formik, Form, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
+// Formik form validation
 const signUpSchema = Yup.object().shape({
     first_name: Yup.string().required('Required'),
     last_name: Yup.string().required('Required'),
@@ -10,3 +12,39 @@ const signUpSchema = Yup.object().shape({
     email: Yup.string().required('Required'),
     password: Yup.string().required('Required').min(8, 'Password must be at least 8 characters'),
 });
+
+function SignUpForm() {
+
+    const navigate = useNavigate();
+
+    return (
+        <Formik initialValues={{
+            first_name: '',
+            last_name: '',
+            email: '',
+            username: '',
+            password: '',
+        }}
+        validationSchema={signUpSchema}
+            onSubmit={(values, { setSubmitting, setErrors }) => {
+                axios.post('https://dtesting.applikuapp.com/signup/', values)
+            .then(response => {
+                if(response.status === 201){
+                    navigate('/')
+                }
+                setSubmitting(false);
+            })
+            .catch(err => {
+                if(err.response) {
+                    // error code from server
+                    setErrors(err.response.data);
+                }
+                else {
+                    // request did not work logs error message
+                    console.log('Error', err.message);
+                }
+                setSubmitting(false);
+            });
+        }}></Formik>
+    )
+}
